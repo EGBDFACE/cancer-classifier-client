8c29bf0e64f1543d565b49a97a446d44837e9cb3
JackChuChou <3212497319@qq.com>
2019-07-04 10:16:51 +0800

    add branch deploy

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import Loadable from 'react-loadable';
 import '@/css/base.scss';
 import Home from '../pages/Home';
-import RunModel from '../pages/RunModel';
-import { Route, Router } from 'react-router-dom';
+// import RunModel from '../pages/RunModel';
+// import { Route, Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router} from 'react-router-dom';
 import history from './history';
 
 // const SignIn = Loadable({
@@ -14,9 +15,14 @@ import history from './history';
 //     loader: () => import('../pages/SignUp'),
 //     loading: () => <div className='loadingPage'><div className='sk-rotating-plane'></div></div>
 // });
+const RunModel = Loadable({
+    loader: () => import('../pages/RunModel'),
+    loading: () => <div className='loadingPage'><div className='sk-rotating-plane'></div></div>
+});
 
 export default (
-    <Router history={history}>
+    // <Router history={history}>
+    <Router basename='/cancer-classifier'>
         <div>
             <Route exact path="/" component={Home}/>
             <Route path="/runModel" component={RunModel}/>
