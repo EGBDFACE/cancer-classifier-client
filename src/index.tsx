bcd91074bb3eac35797b2399510bf13912c183bd
JackChu <jackchu@JackChudeiMac.local>
2019-12-12 19:44:02 +0800

    update model css

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { syncHistoryWithStore } from 'react-router-redux';
+// import { syncHistoryWithStore } from 'react-router-redux';
 import routes from 'src/routes';
 import configureStore from 'src/redux/configureStore';
 import 'src/index.scss';
 import DevTools from 'src/redux/devTools';
-import browserHistory from 'src/routes/history';
+// import browserHistory from 'src/routes/history';
 
 export const store = configureStore();
 // export const _history = syncHistoryWithStore(browserHistory, store);
